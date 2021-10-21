b7fdb8e65f0cff3b65b0f50bc078cf395faa91c1
Colin MacGiollaEain <colin@arista.com>
2021-10-21 19:14:15 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # cvp-scripts
+# Overview
 Collection of scripts for use with Arista's CloudVision Portal [CVP]
+
+# Script Details
+## cvp-user-cleanup
+### Problem Statement
+
+### Usage
+
+### Requires
+
+### Tested Python Versions
+* 2.7
+* 3.8
---
+++ cvp-user-cleanup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+# Copyright (c) 2021 Arista Networks, Inc.  All rights reserved.
+# Arista Networks, Inc. Confidential and Proprietary.
+
+# Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+# * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+# * Redistributions in binary form must reproduce the above copyright notice,  this list of conditions and the following disclaimer in the documentation 
+#   and/or other materials provided with the distribution.
+# * Neither the name of the Arista nor the names of its contributors may be used to endorse or promote products derived from this software without 
+#   specific prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS
+# BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+# GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+# DAMAGE.
+
+
+import logging
+import sys, ssl
+import time
+from cvprac.cvp_client import CvpClient
+from getpass import getpass
+
+if ((sys.version_info.major == 3) or
+    (sys.version_info.major == 2 and sys.version_info.minor == 7 and sys.version_info.micro >= 5)):
+    ssl._create_default_https_context = ssl._create_unverified_context
+
+def main():
+
+
+    sys.exit(0)
+
+
+
+if __name__ == "__main__":
+    main()
+    
